907cb46326df6759bdbadbfa5e07d5680ae38157
oceanzhangyang <zhang51848254@163.com>
2022-09-04 21:32:22 +0800

    state test

--- src/components/test/test.jsx
+++ src/components/test/test.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Test extends Component {
-  constructor(props){
-    super(props)
 
-    this.state = {isHot:true}
+  state = {isHot:true}
+
+  changeWeather = ()=>{
+    // console.log(this)
+    const {isHot} = this.state
+    this.setState({isHot:!isHot})
   }
 
   render() {
     const { isHot} = this.state
     return (
       <div>
-        <h2>今天天气很{isHot?'炎热':'凉爽'}</h2>
+        <h2 onClick={this.changeWeather}>今天天气很{isHot?'炎热':'凉爽'}</h2>
       </div>
     )
   }
